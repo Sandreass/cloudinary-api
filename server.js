fdca2f32051995030f30b361e1badb0ef60be25b
Andrei Stroianetki <sandreasweb@gmail.com>
2024-06-13 15:50:53 +0200

    Supplement5

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
-import connectDB from "./database/connectDB.js"
+import connectDB from "./database/connectDB.js";
 import productRouter from "./routes/productRoutes.js";
 
 const connectionString = process.env.MONGO_URL;
@@ -10,26 +10,31 @@ const connectionString = process.env.MONGO_URL;
 const port = process.env.PORT;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("API is running...");
+  res.send("API is running...");
 });
 
 app.use("/products", productRouter);
 const startServer = async () => {
-    try {
-      await connectDB(connectionString);
-      console.log("verbindung mit MONGODB hat geklaptt!");
-      //
-      app.listen(port, () => {
-        console.log(`Port läuft auf Port: ${port}`);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  startServer();
+  try {
+    await connectDB(connectionString);
+    console.log("verbindung mit MONGODB hat geklaptt!");
+    //
+    app.listen(port, () => {
+      console.log(`Port läuft auf Port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
